6e0b4f9c05378dedc5a88cde3b115c21e9a3971a
Dennis Vo <dennisphuc@gmail.com>
2025-11-05 22:50:59 +0100

    feat: finish handling clicking title and pick and switch player turn

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Tile.css";
 
-const Tile = ({ className }) => {
-  return <div className={`tile ${className}`}>X</div>;
+const Tile = ({ className, value, onClick }) => {
+  return (
+    <div className={`tile ${className}`} onClick={onClick}>
+      {value}
+    </div>
+  );
 };
 
 export default Tile;
--- src/pages/TicTacToe/TicTacToe.jsx
+++ src/pages/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "../../components/Board/Board";
 
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+
 const TicTacToe = () => {
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+
+  const handleTileClick = (index) => {
+    const newTiles = [...tiles];
+    newTiles[index] = playerTurn;
+    setTiles(newTiles);
+    if (playerTurn === PLAYER_X) {
+      setPlayerTurn(PLAYER_O);
+    } else {
+      setPlayerTurn(PLAYER_X);
+    }
+  };
+
   return (
     <div>
       <h1>TicTacToe</h1>
-      <Board />
+      <Board tiles={tiles} onTileClick={handleTileClick} />
     </div>
   );
 };
